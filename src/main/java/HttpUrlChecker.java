4b52eb269db9984b913b30addeacd7f533b236dd
leon.schloemmer <leon.schloemmer@gmail.com>
2018-04-25 22:41:06 +0200

    Edited HttpUrlChecker.java

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -1,6 +1,6 @@
 /*
- * Copyright ©2018. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
- * Department of Informatics and Media Technique, HTBLA Leonding,
+ * Copyright ©2016. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
+ * Department of Informatics and Media Technique, HTBLA Leonding, 
  * Limesstr. 12 - 14, 4060 Leonding, AUSTRIA. All Rights Reserved. Permission
  * to use, copy, modify, and distribute this software and its documentation
  * for educational, research, and not-for-profit purposes, without fee and
@@ -9,21 +9,42 @@
  * appear in all copies, modifications, and distributions. Contact the Head of
  * Informatics and Media Technique, HTBLA Leonding, Limesstr. 12 - 14,
  * 4060 Leonding, Austria, for commercial licensing opportunities.
- *
+ * 
  * IN NO EVENT SHALL HTBLA LEONDING BE LIABLE TO ANY PARTY FOR DIRECT,
  * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
  * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
  * EVEN IF HTBLA LEONDING HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * HTBLA LEONDING SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  * PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
  * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
  * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
+package httpurl;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ *
+ * @author P. Bauer (p.bauer@htl-leonding.ac.at)
+ */
 public class HttpUrlChecker {
-
+    
+    private Pattern p;
+    private Matcher m;
+    
+    public HttpUrlChecker(){
+        p = Pattern.compile("");
+    }
+    
+    //public Matcher getMatcher(String s){
+    //    return p.matcher(s);
+    //}
+    
+    public static Matcher getMatcher(String s){
+        Pattern p = Pattern.compile("(https?:\\/\\/([^\\-][a-zA-Z\\-1-9]*\\.)*[^\\-][a-zA-Z\\-1-9]*[^\\-]\\.[a-zA-Z]+)");
+        return p.matcher(s);
+    }
 }
